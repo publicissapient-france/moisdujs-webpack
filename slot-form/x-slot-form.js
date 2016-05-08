307f883d984369b72326cb7a0acd2fd911bc63c6
Abdelhakim Bachar <abachar@xebia.fr>
2016-05-08 19:11:26 +0200

    Version initiale de l'application du mois de JS

---
+++ slot-form/x-slot-form.js
@@ -0,0 +1,41 @@
+(function() {
+
+  const owner = document.currentScript.ownerDocument;
+
+  class XSlotForm extends HTMLElement {
+    createdCallback() {
+      this.setupComponent();
+    }
+
+    setupComponent() {
+      const template = owner.querySelector('#x-slot-form');
+      const clone = template.content.cloneNode(true);
+      this.appendChild(clone);
+
+      this.slotColumn = this.querySelector('#slot-column');
+      this.slotName = this.querySelector('#slot-name');
+
+      this.querySelector('#add-slot')
+        .addEventListener('click', this.onClickAddSlot.bind(this), false);
+    }
+
+    onClickAddSlot() {
+      const event = new CustomEvent('add-slot', {
+        detail: {
+          slot: this.slotColumn.value,
+          name: this.slotName.value
+        }
+      });
+      this.dispatchEvent(event);
+      this.reset();
+    }
+
+    reset() {
+      this.slotColumn.value = '';
+      this.slotName.value = '';
+    }
+  }
+
+  document.registerElement('x-slot-form', XSlotForm);
+
+})();
