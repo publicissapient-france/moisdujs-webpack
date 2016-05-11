0881919f510efece119f30e06a4c2e021ed8a473
ModuloM <modulom@modulom.net>
2016-05-11 20:34:06 +0200

    Move js code from index html file to app js file

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-/**
- * Created by modulom on 11/05/2016.
- */
+(function () {
+    const slots = {1: [], 2: [], 3: [], 4: []};
+    const grid = document.querySelector('x-slot-grid');
+
+    document
+      .querySelector('x-slot-form')
+      .addEventListener('add-slot', function (event) {
+        slots[event.detail.slot].push(event.detail.name);
+        grid.setAttribute('data', JSON.stringify(slots));
+      }, true);
+})();
